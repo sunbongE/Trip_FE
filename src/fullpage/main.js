03e0870fec35eb9ec39adf6aeef5c6ebfaf132d6
박태호 <qkrxogh7@naver.com>
2023-11-11 23:42:37 +0900

    fullpage: main.js 수정

--- src/fullpage/main.js
+++ src/fullpage/main.js
@@ -5,8 +5,6 @@ $(function () {
         anchors: ['section1', 'section2', 'section3', 'section4', 'section5'],
         // autoScrolling: true,
         // scrollHorizontally: true,
-        navigation: true,
-        navigationPosition: 'right',
         sectionsColor: ['#ccc', '#bbb', '#ddd', '#eee', '#fff'],
         // afterSlideLoad: function (anchorLink, index, slideAnchor, slideIndex) {
         //     console.log("현재 슬라이드 번호는 " + index);
