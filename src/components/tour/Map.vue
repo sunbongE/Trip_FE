f72e94e5ea6a98c908ade650ec6d6947d809d6bb
306yyy <306yyy@naver.com>
2023-11-12 16:31:30 +0900

    feat : KAKAO MAP 연결

--- src/components/tour/Map.vue
+++ src/components/tour/Map.vue
@@ -1,17 +1,250 @@
 <script setup>
 import { ref, onMounted } from 'vue';
 import { getSidoList, getGugunList, getSearchTourinfo } from "@/api/tour";
+
+defineProps({
+    dataList: Object,
+})
+
+
 </script>
 <template>
     <section>
         <!-- kakao map -->
         <div id="mapBox">
             <div id="map"></div>
+            {{ dataList.value }}
         </div>
     </section>
 
 </template>
 
 <style scoped>
+
+.box {
+  margin-bottom: 70px;
+}
+
+#top-section {
+  box-sizing: border-box;
+  width: 80%;
+  max-width: 1200px;
+  margin: 0 auto;
+  padding-top: 220px;
+}
+
+.ment {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  animation: fadein 3s;
+}
+
+.ment h2 {
+  font-size: 3em;
+  font-weight: 600px;
+}
+
+#mapBox {
+  margin-inline: auto;
+  width: 100%;
+  height: 600px;
+  margin-bottom: 100px;
+}
+#map {
+  width: 100%;
+  height: 100%;
+}
+
+#youtubeBox {
+  width: 100%;
+  padding: 200px 0;
+  background: #f9fbfc;
+}
+
+#youtubeBox p {
+  margin-bottom: 10px;
+  text-align: center;
+  font-size: 3em;
+  font-weight: 700;
+}
+
+#youtubeBox p:nth-child(2) {
+  font-size: 1.4em;
+  font-weight: 600px;
+  color: #8b95a1;
+}
+
+#btnBox {
+  /* display: none; */
+  padding-top: 100px;
+  padding-bottom: 50px;
+  display: flex;
+  justify-content: center;
+}
+
+#btnBox button {
+  color: #fff;
+  font-weight: bold;
+  width: 120px;
+  height: 50px;
+  border-radius: 10px;
+  background: #772cf0;
+  border: none;
+  font-size: 1.2em;
+}
+
+#btnBox button:hover {
+  cursor: pointer;
+}
+
+@keyframes fadein {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
+.selectBox option {
+  padding: 10px;
+  box-sizing: border-box;
+  width: 25%;
+  height: 40px;
+  border-radius: 6px;
+}
+.selectBox {
+  padding: 10px;
+  box-sizing: border-box;
+  width: 25%;
+  height: 50px;
+  border-radius: 6px;
+  background-color: #f2f2f2;
+  border: none;
+  font-size: 1em;
+}
+
+#search-form {
+  display: flex;
+  margin-inline: auto;
+  justify-content: space-evenly;
+  margin-bottom: 30px;
+  width: 80%;
+  box-sizing: border-box;
+}
+
+#search-form input[type="button"] {
+  box-sizing: border-box;
+  width: 10%;
+  padding: 10px;
+  height: 50px;
+  border-radius: 6px;
+  border: none;
+  background-color: #772cf0;
+  color: #fff;
+  font-size: 1em;
+}
+
+.wrap {
+  position: absolute;
+  left: 0;
+  bottom: 40px;
+  width: 288px;
+  height: 132px;
+  margin-left: -144px;
+  text-align: left;
+  overflow: hidden;
+  font-size: 12px;
+  font-family: "Malgun Gothic", dotum, "돋움", sans-serif;
+  line-height: 1.5;
+}
+.wrap * {
+  padding: 0;
+  margin: 0;
+}
+.wrap .info {
+  width: 286px;
+  height: 120px;
+  border-radius: 5px;
+  border-bottom: 2px solid #ccc;
+  border-right: 1px solid #ccc;
+  overflow: hidden;
+  background: #fff;
+}
+.wrap .info:nth-child(1) {
+  border: 0;
+  box-shadow: 0px 1px 2px #888;
+}
+.info .title {
+  padding: 5px 0 0 10px;
+  height: 30px;
+  background: #eee;
+  border-bottom: 1px solid #ddd;
+  font-size: 18px;
+  font-weight: bold;
+}
+.info .close {
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  color: #888;
+  width: 17px;
+  height: 17px;
+  background: url("https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/overlay_close.png");
+}
+.info .close:hover {
+  cursor: pointer;
+}
+.info .body {
+  position: relative;
+  overflow: hidden;
+}
+.info .desc {
+  position: relative;
+  margin: 13px 0 0 90px;
+  height: 75px;
+}
+.desc .ellipsis {
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+}
+.desc .jibun {
+  font-size: 11px;
+  color: #888;
+  margin-top: -2px;
+}
+.info .img {
+  position: absolute;
+  top: 6px;
+  left: 5px;
+  width: 73px;
+  height: 71px;
+  border: 1px solid #ddd;
+  color: #888;
+  overflow: hidden;
+}
+.info:after {
+  content: "";
+  position: absolute;
+  margin-left: -12px;
+  left: 50%;
+  bottom: 0;
+  width: 22px;
+  height: 12px;
+  background: url("https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/vertex_white.png");
+}
+.info .link {
+  color: #5085bb;
+}
+
+/* tablet */
+@media screen and (max-width: 810px) {
+  .ment h2 {
+    font-size: 2.2em;
+    font-weight: 600px;
+  }
+}
 
 </style>
