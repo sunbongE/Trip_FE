e300a2b5787e5bbf3610011981aa4a86de959528
박태호 <qkrxogh7@naver.com>
2023-11-18 15:58:16 +0900

    refactor: 로그인 함수 호출부분 수정 로그인 jwt기반 구현중

--- src/components/member/MemberLoginView.vue
+++ src/components/member/MemberLoginView.vue
@@ -1,60 +1,61 @@
 <script setup>
-import { ref, watch } from "vue";
-import { useRouter, useRoute } from 'vue-router';
+import { ref } from "vue";
+import { useRouter } from 'vue-router';
 import { login } from "@/api/member";
-
+import { storeToRefs } from "pinia";
 const member = ref({
-    userId: "",
-    userPassword: "",
+	userId: "",
+	userPassword: "",
 });
 
-function onSubmit() {
-    // 회원가입으로 호출
-    calllogin();
-}
+
+
+const loginFunc = async () => {
+	console.log("login ing!!!! !!!");
+	await userLogin(loginUser.value);
+	let token = sessionStorage.getItem("accessToken");
+	console.log("111. ", token);
+	console.log("isLogin: ", isLogin);
+	if (isLogin) {
+		console.log("로그인 성공아닌가???");
+		getUserInfo(token);
+		changeMenuState();
+	}
+	router.push("/");
+};
 
 function calllogin() {
-    console.log("로그인 시도", member.value.userId);
-  // API 호출
-  login(member);
+	console.log("로그인 시도", member.value.userId);
+	// API 호출
+	login(member);
 }
 
 const router = useRouter();
-const route = useRoute();
 // ID 찾기
 function moveFindId() {
-  router.push({ name: "findid" });
+	router.push({ name: "findid" });
 }
 
 // PW 찾기
 function moveFindPw() {
-  router.push({ name: "findpw" });
+	router.push({ name: "findpw" });
 }
 </script>
 
 <template>
-    <section>
+	<section>
 		<div class="auth-top">
-		
+
 			<h2>로그인</h2>
 			<h3>반갑습니다! 즐겁게 여행떠날 준비가 되었나요?</h3>
 		</div>
 		<!-- form -->
-		<form @submit.prevent="onSubmit">
-			<!-- <div style="display:flex; justify-content:end;">
-			<c:if test="${empty ck }">
-				<input type="checkbox" id="saveId" name="saveId" value='ok' /> 
-			</c:if>
-			<c:if test="${not empty ck }">
-				<input type="checkbox" id="saveId" name="saveId" value='ok' checked=true/> 
-			</c:if>
-				<label for="saveId">아이디 저장</label>
-					
-			</div> -->
+		<form @submit.prevent="calllogin">
+
 			<div class="auth-input-box">
 				<div class="input-title">아이디</div>
 				<input type="text" v-model="member.userId" required autofocus />
-				
+
 			</div>
 			<div class="auth-input-box">
 				<div class="input-title">비밀번호</div>
@@ -62,20 +63,16 @@ function moveFindPw() {
 			</div>
 			<!-- 버튼 영역 -->
 			<div class="auth-btn-box">
-				<button class="btn btn-dark" type="submit" value="가입하기" id="signup-btn">로그인</button>
+				<button class="btn btn-dark" @click="calllogin">로그인</button>
 			</div>
-    <div>
-        <button class="btn btn-dark" type="button" @click="moveFindId">ID 찾기</button>
-        <button class="btn btn-dark" type="button" @click="moveFindPw">PW 찾기</button>
-    </div>
-			<!-- <a href="${root}/member?action=joinForm" id="withdraw-btn"><p>회원이
-					아니신가요? 여기를 클릭해주세요.</p></a> -->
+			<div>
+				<button class="btn btn-dark" type="button" @click="moveFindId">ID 찾기</button>
+				<button class="btn btn-dark" type="button" @click="moveFindPw">PW 찾기</button>
+			</div>
 		</form>
 	</section>
-
 </template>
 
 <style scoped>
 @import "@/assets/sass/member/login.scss";
- 
 </style>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 import App from './App.vue'
 import router from './router'
-import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+
 const app = createApp(App)
 const pinia = createPinia()
+
 pinia.use(piniaPluginPersistedstate)
+app.use(pinia);
 app.use(createPinia())
-// app.use(metaManager);
 app.use(router)
 app.mount('#app')
