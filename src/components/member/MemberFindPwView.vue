b9e68905cf44a64383e8dea9aa3d4068004b3c55
박태호 <qkrxogh7@naver.com>
2023-11-20 11:36:54 +0900

    feat:회원가입, 아이디/비밀번호 찾기 FE / api연결

--- src/components/member/MemberFindPwView.vue
+++ src/components/member/MemberFindPwView.vue
@@ -1,7 +1,8 @@
 <script setup>
 import { ref, watch } from "vue";
 import { findpw } from "@/api/member";
-
+import { useRouter } from 'vue-router';
+const router = useRouter(); 
 const member = ref({
   userId: "",
   emailId: "",
@@ -9,30 +10,37 @@ const member = ref({
 });
 
 function onSubmit() {
-  // 회원가입으로 호출
   callfindpw();
 }
 
 function callfindpw() {
-  console.log("비밀번호 찾기 슛", member.value.userId);
   // API 호출
-  findpw(member);
+	findpw(member,
+		({data}) => {
+			alert("임시 비밀번호 발급 : " + data)
+			router.push("login")
+		},
+		(error) => {
+			alert("일치하는 회원 정보가 없습니다.")
+		
+	});
 }
 
 
 </script>
 
 <template>
-    <section>
+    <div id='frame'>
+		<div id="formBox">
+
 		<div class="auth-top">
-		
 			<h2>PW 찾기 페이지</h2>
 			<h3>패스워드를 잊어버리셨나요 ?</h3>
 		</div>
 		<!-- form -->
 		<form @submit.prevent="onSubmit">
 			<div class="auth-input-box">
-				<div class="input-title">이름</div>
+				<div class="input-title">아이디</div>
 				<input type="text" v-model="member.userId" required autofocus />
 				
 			</div>
@@ -40,18 +48,20 @@ function callfindpw() {
 				<div class="input-title">이메일</div>
 				<input type="text" v-model="member.emailId" required />
 			</div>
-      <div class="auth-input-box">
+
+      		<div class="auth-input-box">
 				<div class="input-title">생년월일</div>
 				<input type="text" v-model="member.birth" required />
 			</div>
 			<!-- 버튼 영역 -->
-			<div class="auth-btn-box">
-				<button type="submit" id="signup-btn">찾기</button>
+			<div class='btnBox'>
+				<button type="submit" class="okBtn">찾기</button>
 			</div>
-			<!-- <a href="${root}/member?action=joinForm" id="withdraw-btn"><p>회원이
-					아니신가요? 여기를 클릭해주세요.</p></a> -->
-		</form>
-	</section>
+				</form>
+		</div>
+	
+	</div>
+
 </template>
 
 <style scoped>
