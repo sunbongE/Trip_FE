97cf4dbf5b357a1cd7c8bdd1d218a55c93c95718
박태호 <qkrxogh7@naver.com>
2023-11-22 10:16:46 +0900

    feat : 여행계획 구현주웅

---
+++ src/components/plan/PlanWrite.vue
@@ -0,0 +1,38 @@
+<script setup>
+
+</script>
+
+<template>
+   <section>
+
+<article>
+  <div>
+    <h2 id="ment">여행계획 작성</h2>
+
+  </div>
+  <form id="articleForm" method="post">
+    <input type="hidden" name="action" value="regist">
+    <div id="formBox">
+      <input type="hidden" id="userId" name="userId" readonly required="required"  />
+  
+
+      <label for="subject">여행 제목</label>
+      <input type="text" id="subject" name="subject" autocomplete="off"  />
+      <label for="content">질문 내용</label>
+      <textarea id="content" name="content" cols="50" rows="20" required="required"
+        ></textarea>
+
+
+    </div>
+    <div id="btnBox">
+      <button type="button">닫기</button>
+      <button type="button">작성하기</button>
+    </div>
+  </form>
+</article>
+</section>
+</template>
+
+<style scoped>
+@import '@/assets/sass/plan/planWrite.scss';
+</style>
--- src/views/PlanView.vue
+++ src/views/PlanView.vue
@@ -1,13 +1,43 @@
 <script setup>
-import PlanWrite from "@/commponents/plan/PlanWrite.vue"
+import PlanWrite from "../components/plan/PlanWrite.vue"
+import { useTourPlanStore } from "@/stores/tourPlan.js"
+import { storeToRefs } from 'pinia';
+const tourPlanStore = useTourPlanStore();
+const { planListCnt, planList } = storeToRefs(tourPlanStore);
+
 </script>
 
 <template>
-    <div>
-        <router-view>PlanWrite</router-view>
+    <div id='planFrame'>
+        <h2>여행지 목록</h2>
+        <hr>
+        <div id='tourList'>
+            <template v-for='info in planList'>
+                <div class="showPlanFrame">
+                    <div id="myList">
+                    <div id="myListFrame">
+                        <div class="Images">
+                            <img v-if="info.firstImage==='' " src="../assets/images/empty.png" alt="">
+                            <img v-else :src="info.firstImage" alt="" srcset="">
+                        </div>
+                        <div class="info">
+                            <div class="infoTitle">
+                                <span>여행지 : {{ info.title }}</span>
+                            </div>
+                            <div>
+                                <p>주소: {{ info.addr }}</p>
+                                <p>전화번호: {{ info.tel }}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </template>
+        </div>
+        <PlanWrite></PlanWrite>
     </div>
 </template>
 
 <style scoped>
-
+@import '@/assets/sass/plan/plan.scss';
 </style>
