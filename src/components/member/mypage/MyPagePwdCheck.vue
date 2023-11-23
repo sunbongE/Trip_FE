cdcb805378e1efdf94c93c13c72a9f4a91a5aeb7
306yyy <306yyy@naver.com>
2023-11-24 02:28:26 +0900

    feat : css 추가

--- src/components/member/mypage/MyPagePwdCheck.vue
+++ src/components/member/mypage/MyPagePwdCheck.vue
@@ -7,6 +7,7 @@ const memberStore = useMemberStore();
 
 const inputpassword = ref("");
 const isCorrect = ref(false);
+const isInputed = ref(true);
 
 function pwdCheck() {
   console.log(memberStore.userInfo.userId);
@@ -20,6 +21,7 @@ function pwdCheck() {
           if (data) {
               console.log("PW CHECK RESULT : ", data);
               isCorrect.value = data;
+              isInputed.value = false;
           } else {
               alert("비밀번호가 틀렸습니다.\n확인 후 다시 입력 바랍니다.");
           }
@@ -63,12 +65,13 @@ const userDto = ref({
 
 <template>
   <article>
-    <div>
+    <div class="modify-container">
       <h2 id="ment">회원 정보 수정</h2>
       <h2 id="subment">필요한 회원 정보를 수정해주세요</h2>
-      <label>비밀번호 입력</label>
-      <input type="password" v-model="inputpassword" @keyup.enter="pwdCheck" />
-
+      <div v-show="isInputed">
+        <label>비밀번호 입력</label>
+        <input type="password" v-model="inputpassword" @keyup.enter="pwdCheck" />
+      </div>
       <div v-show="isCorrect">
         <form id="articleForm" @submit.prevent="">
           <input type="hidden" name="articleNo" :value="memberStore.userInfo.userId" />
@@ -99,7 +102,111 @@ const userDto = ref({
 <style scoped>
 @import "@/assets/sass/board/BoardForm.scss";
 
-#emailBox {
-  display: flex;
+article {
+    display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+    width: 100%;
+    animation: fadein 3s;
+  }
+  
+  #ment {
+    font-size: 2.6em;
+    font-weight: 700;
+  }
+  
+  #subment {
+    font-size: 1.2em;
+    font-weight: 600;
+    margin-top: 10px;
+    color: #8b95a1;
+  }
+  
+  #formBox {
+    width: 100%;
+    padding: 50px 0;
+  }
+  
+  #articleForm {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+  }
+  
+  #articleForm label {
+    color: #6f6f6f;
+  }
+  
+  #articleFrom input:nth-child(1) {
+    border-radius: 6px;
+    border: 1px solid #d9d9d9;
+    background: #f2f2f2;
+  }
+  
+  #articleForm input {
+    box-sizing: border-box;
+    border: 1px solid #d9d9d9;
+    width: 100%;
+    height: 40px;
+    border-radius: 6px;
+    padding: 10px;
+  }
+  
+  #articleForm textarea {
+    border: 1px solid #d9d9d9;
+    box-sizing: border-box;
+    width: 100%;
+    border-radius: 6px;
+    padding: 10px;
+    resize: none;
+  }
+  
+  #articleForm label {
+    margin: 20px 0 5px 0;
+  }
+  
+  #btnBox {
+    margin-bottom: 50px;
+    display: flex;
+    justify-content: center;
+  }
+  
+  #btnBox button {
+    margin: 0 10px;
+    width: 120px;
+    height: 50px;
+    border-radius: 6px;
+    background: #f7f8fa;
+    border: 1px solid #d9d9d9;
+    font-size: 1.1em;
+  }
+  
+  #btnBox button:hover {
+    cursor: pointer;
+  }
+  
+  #btnBox button:nth-child(2) {
+    color: #fff;
+    background: #51abf3;
+    border: 1px solid #51abf3;
+  }
+  #imgBox{
+    display: flex;
+    flex-direction: row;
+    height: 450px;
+    width: 100%;
+    grid-gap: 2em;
+    overflow: scroll;
+  }
+
+  img{
+    height: 400px;
+    width: 400px;
+  }
+
+#modify-container {
+    width : 80%;
 }
 </style>
